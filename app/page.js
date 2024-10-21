d43af77e58d0675448e34b4ccff846f6384ee607
patlim <125264372+patlim@users.noreply.github.com>
2024-10-21 14:09:32 +0200

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,120 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const Header = styled.header`
+  display: flex;
+  padding: 24px;
+  .count {
+    color: #49454f50;
+  }
+`;
+
+const CardContainer = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 56px 24px 112px 24px;
+  transition: all 0.5s ease;
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 12px;
+  border: 1px solid #CAC4D0;
+  background: #FEF7FF;
+  overflow: hidden;
+
+  img {
+    height: 250px;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+const StatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+  gap: 12px;
+`;
+
+const Stat = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.p`
+  font-weight: bold;
+`;
+
+const Home = () => {
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    const endpoint = "https://api.thedogapi.com/v1/breeds";
+    axios.get(endpoint).then((res) => {
+      console.log(res.data);
+      setBreeds(res.data);
+    });
+  }, []);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="page">
+      <Header>
+        {breeds.length > 0 && (
+          <h1>
+            Breeds <span className='count'>{breeds.length}</span>
+          </h1>
+        )}
+      </Header>
+      <CardContainer>
+        {breeds.map((breed) => (
+          <Card className="card" key={breed.id}>
+            <img
+              src={`https://cdn2.thedogapi.com/images/${breed.reference_image_id}.jpg`}
+              alt={breed.name}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <StatContainer>
+              <Stat>
+                <Label>Weight</Label>
+                <p>{breed.weight.metric} kg</p>
+              </Stat>
+              <Stat>
+                <Label>Breed</Label>
+                <p>{breed.name}</p>
+              </Stat>
+              <Stat>
+                <Label>Temperament</Label>
+                <p>{breed.temperament}</p>
+              </Stat>
+              <Stat>
+                <Label>Origin</Label>
+                {breed.origin 
+                  ? <p>{breed.origin}</p>
+                  : <p>-</p>
+                }
+              </Stat>
+              <Stat>
+                <Label>Life Span</Label>
+                <p>{breed.life_span}</p>
+              </Stat>
+            </StatContainer>
+          </Card>
+        ))}
+      </CardContainer>
     </div>
   );
-}
+};
+
+export default Home;

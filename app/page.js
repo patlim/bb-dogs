2954f28ad3528426ae3b9de9bb35318fdc274cd4
patlim <125264372+patlim@users.noreply.github.com>
2024-10-21 14:39:52 +0200

    reset next config

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { getStaticProps } from "next/dist/build/templates/pages";
 
 const Header = styled.header`
   display: flex;

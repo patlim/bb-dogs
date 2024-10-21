9cef3494b292231b90714b01b0862209a6d93175
patlim <125264372+patlim@users.noreply.github.com>
2024-10-21 14:43:12 +0200

    toml

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import { getStaticProps } from "next/dist/build/templates/pages";
 
 const Header = styled.header`
   display: flex;
